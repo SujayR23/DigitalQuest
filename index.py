a9eb7895b730a1a9b92bf82c5c37048186452653
SujayR23 <138759410+SujayR23@users.noreply.github.com>
2023-11-15 06:34:29 +0530

    Update index.py

--- index.py
+++ index.py
@@ -186,4 +186,4 @@ def answer10():
     return render_template('index.html', start= Start, questions= questions, status= 'wrong')
 
 if __name__== '__main__':
-    app.run(debug = True)
+    app.run(debug = False, host='0.0.0.0')
